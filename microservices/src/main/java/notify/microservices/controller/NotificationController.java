2ab1dce915b4e8f548eba6987054b1157786db32
Bryam <donalk100@gmail.com>
2025-06-16 01:45:51 -0500

    cambios
    Dynamodb y librerias, cambios en backend con rds y revisión de estado frontend

--- microservices/src/main/java/notify/microservices/controller/NotificationController.java
+++ microservices/src/main/java/notify/microservices/controller/NotificationController.java
@@ -3,6 +3,7 @@ package notify.microservices.controller;
 import notify.microservices.services.NotificationService;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 
 @RestController
 @RequestMapping("/api/notify")
@@ -12,10 +13,16 @@ public class NotificationController {
     @Autowired
     private NotificationService notificationService;
 
-    @PostMapping("/push")
-    public String sendPush(@RequestParam String token,
-                           @RequestParam String title,
-                           @RequestParam String body) {
-        return notificationService.sendPushNotification(token, title, body);
+    @PostMapping("/send")
+    public ResponseEntity<String> send(@RequestParam String token,
+            @RequestParam String title,
+            @RequestParam String body) {
+        try {
+            String result = notificationService.sendPushNotification(token, title, body);
+            return ResponseEntity.ok(result);
+        } catch (Exception e) {
+            e.printStackTrace(); // 👈 que se vea el error
+            return ResponseEntity.status(500).body("Error interno: " + e.getMessage());
+        }
     }
 }
--- microservices/src/main/java/notify/microservices/services/NotificationService.java
+++ microservices/src/main/java/notify/microservices/services/NotificationService.java
@@ -5,22 +5,35 @@ import com.google.firebase.messaging.FirebaseMessagingException;
 import com.google.firebase.messaging.Message;
 import com.google.firebase.messaging.Notification;
 
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import notify.microservices.entity.NotificationEntity;
 import notify.microservices.repository.NotificationRepository;
+import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.UUID;
+import software.amazon.awssdk.services.dynamodb.model.AttributeValue;
+import software.amazon.awssdk.services.dynamodb.model.PutItemRequest;
 
+import org.checkerframework.checker.units.qual.A;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
-
 @Service
+@AllArgsConstructor
 public class NotificationService {
 
-    @Autowired
-    private NotificationRepository repo;
+    private final DynamoDbClient dynamoDbClient;
+    private final Environment env;
+
+
+
 
     public String sendPushNotification(String token, String title, String body) {
-        UUID userId = UUID.randomUUID(); // Valida formato
+        UUID userId = UUID.randomUUID();
         NotificationEntity notification = new NotificationEntity(null, userId.toString(), title, body, token, false);
 
         try {
@@ -34,11 +47,29 @@ public class NotificationService {
 
             String response = FirebaseMessaging.getInstance().send(message);
             notification.setSent(true);
-            repo.save(notification);
+            saveToDynamo(notification);
             return "Enviado correctamente: " + response;
         } catch (FirebaseMessagingException e) {
-            repo.save(notification);
+            saveToDynamo(notification);
             return "Error al enviar: " + e.getMessage();
         }
+    }
+
+    private void saveToDynamo(NotificationEntity n) {
+            String environment = env.getProperty("aws.dynamodb.table");
+        Map<String, AttributeValue> item = new HashMap<>();
+        item.put("id", AttributeValue.fromS(UUID.randomUUID().toString()));
+        item.put("userId", AttributeValue.fromS(n.getUserId()));
+        item.put("title", AttributeValue.fromS(n.getTitle()));
+        item.put("body", AttributeValue.fromS(n.getBody()));
+        item.put("deviceToken", AttributeValue.fromS(n.getDeviceToken()));
+        item.put("sent", AttributeValue.fromBool(n.isSent()));
+
+        PutItemRequest request = PutItemRequest.builder()
+                .tableName(environment)
+                .item(item)
+                .build();
+
+        dynamoDbClient.putItem(request);
     }
 }

cf6762edd4a37d74f4236fe7fd88425e7cfa3c08
Bryam <donalk100@gmail.com>
2025-06-16 00:37:09 -0500

    cambios en mongodb para guardado

--- microservices/src/main/java/notify/microservices/services/NotificationService.java
+++ microservices/src/main/java/notify/microservices/services/NotificationService.java
@@ -1,26 +1,44 @@
 package notify.microservices.services;
 
 import com.google.firebase.messaging.FirebaseMessaging;
+import com.google.firebase.messaging.FirebaseMessagingException;
 import com.google.firebase.messaging.Message;
 import com.google.firebase.messaging.Notification;
+
+import notify.microservices.entity.NotificationEntity;
+import notify.microservices.repository.NotificationRepository;
+
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class NotificationService {
 
+    @Autowired
+    private NotificationRepository repo;
+
     public String sendPushNotification(String token, String title, String body) {
-        Message message = Message.builder()
-                .setToken(token)
-                .setNotification(Notification.builder()
-                        .setTitle(title)
-                        .setBody(body)
-                        .build())
-                .build();
+        UUID userId = UUID.randomUUID(); // Valida formato
+        NotificationEntity notification = new NotificationEntity(null, userId.toString(), title, body, token, false);
 
         try {
-            return FirebaseMessaging.getInstance().send(message);
-        } catch (Exception e) {
-            return "Error al enviar notificación: " + e.getMessage();
+            Message message = Message.builder()
+                    .setToken(token)
+                    .setNotification(Notification.builder()
+                            .setTitle(title)
+                            .setBody(body)
+                            .build())
+                    .build();
+
+            String response = FirebaseMessaging.getInstance().send(message);
+            notification.setSent(true);
+            repo.save(notification);
+            return "Enviado correctamente: " + response;
+        } catch (FirebaseMessagingException e) {
+            repo.save(notification);
+            return "Error al enviar: " + e.getMessage();
         }
     }
 }
